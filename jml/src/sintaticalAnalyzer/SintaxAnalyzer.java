3c628729489d78cc446a86f09cb6fcff0fd9bec7
tfernandes.cc@gmail.com <tfernandes.cc@gmail.com@1488880e-7340-b142-2708-d90e62f99a39>
2011-06-01 11:45:39 +0000

    começando o analisador sintatico mais uma vez

--- jml/src/sintaticalAnalyzer/SintaxAnalyzer.java
+++ jml/src/sintaticalAnalyzer/SintaxAnalyzer.java
@@ -28,27 +28,49 @@ public class SintaxAnalyzer {
 	 */
 	public static SintaxElement parseLexems(List<Lexem> list) throws Error {
 		
-		
+		if (stack==null)stack=new Stack<SintaxElement>();
+		reconhece_def_local_global(list);
 		
 		return null;
 	}
 	
 	
 	
-	private static boolean reconhece_expressao(List<Lexem> list){
+	private static boolean reconhece_def_local_global(List<Lexem> list){
 		List<SintaxElement> laux=new LinkedList<SintaxElement>();
-		SintaxElement se = new SintaxElement(list.get(0));
-		int i=0;
-		if (stack.size()>0){
-			laux.add(0,stack.pop());
-			if (laux.get(0).getId()==SintaxElementId.KEYWORD_LET){
+		laux.add(new SintaxElement(list.get(0)));
+		
+		if (laux.get(0).getId()==SintaxElementId.KEYWORD_LET){
+				list.remove(0);//remove let da lista
 				
-				while(list.get(i).getId()!){
-				  laux.add(list.remove(0));
+				//detectou-se um let: enquanto exisistir  <id> e <op> empilhe
+				
+				while(new SintaxElement(list.get(0)).getId()==SintaxElementId.ID || new SintaxElement(list.get(0)).getId()==SintaxElementId.ASSIGNMENT || reconhece_e(list)){//cuidado tem que ser <id> seguido de "=", <e> , "in" e <e>
+					laux.add(new SintaxElement(list.remove(0)));//espera-se um <id> ou <op>
+					System.out.println(laux.size()-1+ " "+laux.get(laux.size()-1).getId());
 				}
 				
-			}
+				if (new SintaxElement(list.get(0)).getId()==SintaxElementId.KEYWORD_IN){//seguido de um "in" é definição local
+					laux.add(new SintaxElement(list.remove(0)));
+					if ( reconhece_e(list)){
+						stack.push(new SintaxElement(SintaxElementId.DEFINICAO_LOCAL,laux));
+						return true;
+					}
+				}else{
+					if ( reconhece_e(list)){
+						stack.push(new SintaxElement(SintaxElementId.DEFINICAO_GLOBAL,laux));
+						return true;
+					}
+				}
 		}
+		return false;
+	}
+	
+	//tive a ideia de ir empilhando enquanto nao for nenhuma definição da gramatica e qnd chegar numa monta um metodo que reconhce o que tem na lista
+	private static boolean reconhece_e(List<Lexem> list){
+		
+		//if 
+		return false;
 	}
 	
 	
