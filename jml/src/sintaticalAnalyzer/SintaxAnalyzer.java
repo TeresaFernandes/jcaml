09fe27bee990730313e297c7daa38d75ab08b2c9
tfernandes.cc@gmail.com <tfernandes.cc@gmail.com@1488880e-7340-b142-2708-d90e62f99a39>
2011-06-02 11:26:20 +0000

    analisador sintatico parece está dando certo agora

--- jml/src/sintaticalAnalyzer/SintaxAnalyzer.java
+++ jml/src/sintaticalAnalyzer/SintaxAnalyzer.java
@@ -34,29 +34,27 @@ public class SintaxAnalyzer {
 				
 		if (reconhece_definicao_local());
 		return null;
+		
+		
 	}
 	
 	
 	private static boolean reconhece_definicao_local(){
 		List<SintaxElement> laux=new LinkedList<SintaxElement>();
-		
 		if (list.size()>5){
-		
-			if (new SintaxElement(list.get(0)).getId() == SintaxElementId.KEYWORD_LET 
-					&& new SintaxElement(list.get(1)).getId() == SintaxElementId.ID
-					&& new SintaxElement(list.get(2)).getId() == SintaxElementId.ASSIGNMENT
+			
+			if (	reconhece_Let()
+					&& reconhece_ID()
+					&& reconhece_Assigment()
 					&& reconhece_e()
-					&& new SintaxElement(list.get(0)).getId() == SintaxElementId.KEYWORD_IN
+					&& reconhece_In()
 					&& reconhece_e()){
-					
-				laux.add(new SintaxElement(list.remove(0)));
-				laux.add(new SintaxElement(list.remove(0)));
-				laux.add(new SintaxElement(list.remove(0)));
-				laux.add(stack.pop());//lista retornada por reconhece_e que esta na pilha
-				laux.add(new SintaxElement(list.remove(0)));
-				laux.add(stack.pop());//lista retornada por reconhece_e que esta na pilha
 				
-				stack.push(new SintaxElement(SintaxElementId.DEFINICAO_LOCAL, laux));
+				for (int i=0;i<6;i++){
+					laux.add(stack.pop());	
+				}
+				
+				stack.push(new SintaxElement(SintaxElementId.DEFINICAO_LOCAL, laux));	
 				return true;
 			}
 		}
@@ -64,90 +62,66 @@ public class SintaxAnalyzer {
 			return false;
 	}
 	
+	private static boolean reconhece_Let(){
+		SintaxElement se = new SintaxElement(list.remove(0));
+		if (se.getId() == SintaxElementId.KEYWORD_LET){
+			
+			stack.push(se);
+			return true;
+		}
+		list.add(0, se.getLexem());
+		return false;
+	}
+	private static boolean reconhece_ID(){
+		SintaxElement se = new SintaxElement(list.remove(0));
+		if (se.getId() == SintaxElementId.ID){
+			stack.push(se);
+			return true;
+		}
+		list.add(0, se.getLexem());
+		return false;
+	}
+	private static boolean reconhece_Assigment(){
+		SintaxElement se = new SintaxElement(list.remove(0));
+		if (se.getId() == SintaxElementId.ASSIGNMENT){
+			stack.push(se);
+			return true;
+		}
+		list.add(0, se.getLexem());
+		return false;
+	}
+	private static boolean reconhece_In(){
+		SintaxElement se = new SintaxElement(list.remove(0));
+		if (se.getId() == SintaxElementId.KEYWORD_IN){
+			stack.push(se);
+			return true;
+		}
+		list.add(0, se.getLexem());
+		return false;
+	}
+	
 	private static boolean reconhece_e(){
 		List<SintaxElement> laux=new LinkedList<SintaxElement>();
 		
-		if (new SintaxElement(list.get(0)).getId() == SintaxElementId.ID
-				||new SintaxElement(list.get(0)).getId() == SintaxElementId.CONST){//mais possibilidade aqui
-			laux.add(new SintaxElement(list.remove(0)));
-			while(new SintaxElement(list.get(0)).getId() == SintaxElementId.OP || new SintaxElement(list.get(0)).getId() == SintaxElementId.CONST || new SintaxElement(list.get(0)).getId() == SintaxElementId.ID){//adicionar algo que infira a ordem certa de aparecimento das definições
+			if (new SintaxElement(list.get(0)).getId() == SintaxElementId.ID
+					||new SintaxElement(list.get(0)).getId() == SintaxElementId.CONST
+					||list.get(0).getId()==LexemId.OPERATOR_UNARYMINUS
+					||list.get(0).getId()==LexemId.OPERATOR_NOT){//mais possibilidade aqui
+				
 				laux.add(new SintaxElement(list.remove(0)));
-			}
-			
-			stack.push(new SintaxElement(SintaxElementId.E, laux));
-			return true;
-		}
-		return false;
-	}
-	
-	/*private static boolean reconhece_def_local_global(List<Lexem> list){
-		List<SintaxElement> laux=new LinkedList<SintaxElement>();
-		laux.add(new SintaxElement(list.get(0)));
-		
-		if (laux.get(0).getId()==SintaxElementId.KEYWORD_LET){
-				list.remove(0);//remove let da lista
 				
-				//detectou-se um let: enquanto exisistir  <id> e <op> empilhe
+				while((!list.isEmpty())&& (new SintaxElement(list.get(0)).getId() == SintaxElementId.OP 
+						|| new SintaxElement(list.get(0)).getId() == SintaxElementId.CONST 
+						|| new SintaxElement(list.get(0)).getId() == SintaxElementId.ID)){//adicionar algo que infira a ordem certa de aparecimento das definições
 				
-				while(new SintaxElement(list.get(0)).getId()==SintaxElementId.ID || new SintaxElement(list.get(0)).getId()==SintaxElementId.ASSIGNMENT || reconhece_e(list)){//cuidado tem que ser <id> seguido de "=", <e> , "in" e <e>
-					laux.add(new SintaxElement(list.remove(0)));//espera-se um <id> ou <op>
-					System.out.println(laux.size()-1+ " "+laux.get(laux.size()-1).getId());
-				}
-				
-				if (new SintaxElement(list.get(0)).getId()==SintaxElementId.KEYWORD_IN){//seguido de um "in" é definição local
 					laux.add(new SintaxElement(list.remove(0)));
-					if ( reconhece_e(list)){
-						stack.push(new SintaxElement(SintaxElementId.DEFINICAO_LOCAL,laux));
-						return true;
-					}
-				}else{
-					if ( reconhece_e(list)){
-						stack.push(new SintaxElement(SintaxElementId.DEFINICAO_GLOBAL,laux));
-						return true;
-					}
+					
 				}
-		}
-		return false;
-	}
-	
-	private static boolean reconhece_programa(List<Lexem> list){
-		List<SintaxElement> laux=new LinkedList<SintaxElement>();
-		
-		//if (stack.size()==0) return true;
-		if (stack.size()>0){
-			laux.add(stack.pop());
-			
-			if(laux.get(0).getId()==SintaxElementId.DEFINICAO){
-				stack.push(new SintaxElement(SintaxElementId.PROGRAMA, laux));
-				return true;
-			}else{
-				stack.push(laux.remove(0));
-			}
-		}
-		
-		return false;
-	}	
-	private static boolean reconhece_definicao(List<Lexem> list){
-		List<SintaxElement> laux=new LinkedList<SintaxElement>();
-		
-		if (stack.size()>0){
-			
-			laux.add(0,stack.pop());
-			
-			if (   laux.get(0).getId()==SintaxElementId.CHAMADA_FUNCAO 
-				||  laux.get(0).getId()==SintaxElementId.DEFINICAO_LOCAL
-				||  laux.get(0).getId()==SintaxElementId.DEFINICAO_GLOBAL
-				||  laux.get(0).getId()==SintaxElementId.EXP){
 				
-				stack.push(new SintaxElement(SintaxElementId.DEFINICAO, laux));
+				stack.push(new SintaxElement(SintaxElementId.E, laux));
+
 				return true;
-			}
-		}
-		
-		while(!laux.isEmpty()){
-			stack.push(laux.remove(0));
 		}
 		return false;
 	}
-	*/	
-}
+	}
